dc91d767cf5ce68191340ec5fbd319cb47f9065c
priyankasharma02411 <priyankasharma241189@gmail.com>
2023-02-24 10:32:19 +0530

    New change

---
+++ src/RepositoryOverview.java
@@ -0,0 +1,17 @@
+
+public class RepositoryOverview {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print(true);
+//	    folder path must be "C:\\Users\\C.Tech Computers\\eclipse-workspace\\OCP>"
+//		git pull origin main
+//		git status
+//		git add .
+//		git commit -m "any msg"
+//		git push origin main
+
+	}
+
+}
